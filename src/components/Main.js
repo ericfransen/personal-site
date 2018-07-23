a82f06fd1da4b08645f6cc04fac885b72e13b934
ericfransen <fransenep@gmail.com>
2018-07-23 10:33:47 -0600

    added bio, fixed background overlay

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,16 +13,32 @@ class Main extends React.Component {
     return (
       <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="intro" 
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
+          style={{display:'none'}}>
           <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
           <p>
             Hi, I'm Eric.
+            <br/><br/>
+            I’m a full-stack developer with an emphasis on usability and standards to solve complex problems through cutting-edge technologies and proven conventions.
+            My track record for delivering well-crafted and -designed software showcases my dedication to producing user-focused and performant solutions.
+            <br/><br/>
+            My collegiate career began  at CU studying architecture and quickly pivoted after discovering my passion for healthcare and entrepreneurial pursuits.  
+            Designing and managing the creation of software as well as exposure to the growing startup ecosystem in Colorado 
+            brought me full circle to my true calling of combining the art and science of curating elegant experiences for others.
+            <br/><br/>
+            Now, innovating and disrupting legacy industries allows me to build my craft around what I love most: 
+            transforming ambitious ideas into seamlessly ever-evolving products. 
+            <br/><br/>
+            When I’m not coding, you’ll find me taking advantage of all the majestic activities Colorado has to offer, 
+            from biking and hiking in the summer to skiing and hibernating in the winter.  
           </p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="work" 
+          className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
+          style={{display:'none'}}>
           <h2 className="major">Work</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
           <p>
@@ -31,7 +47,9 @@ class Main extends React.Component {
           {close}
         </article>
 
-        <article id="social" className={`${this.props.article === 'social' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="social" 
+          className={`${this.props.article === 'social' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
+          style={{display:'none'}}>
           <h2 className="major">Social</h2>
           <ul className="icons">
             <li><a href="https://twitter.com/eric_fransen" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
@@ -43,7 +61,9 @@ class Main extends React.Component {
             {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="contact" 
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
+            style={{display:'none'}}>
           <h2 className="major">Contact</h2>
           <form method="post" data-netlify="true" method="POST">
             <div className="field half first">
