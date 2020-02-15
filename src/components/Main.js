e10c809e093073e92ed4c89c611456f29b8e8e8d
ericfransen <fransenep@gmail.com>
2020-02-15 15:06:21 -0700

    RIP google+

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,63 +7,126 @@ import pic03 from '../images/pic03.jpg'
 
 class Main extends React.Component {
   render() {
-
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      ></div>
+    )
 
     return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" 
-          className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
-          style={{display:'none'}}>
+      <div
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="intro"
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Intro</h2>
           <p>
             Hi, I'm Eric.
-            <br/><br/>
-            I’m a full-stack developer with an emphasis on usability and standards to solve complex problems through cutting-edge technologies and proven conventions.
-            My track record for delivering well-crafted and -designed software showcases my dedication to producing user-focused and performant solutions.
-            <br/><br/>
-            My collegiate career began  at CU studying architecture and quickly pivoted after discovering my passion for healthcare and entrepreneurial pursuits.  
-            Designing and managing the creation of software as well as exposure to the growing startup ecosystem in Colorado 
-            brought me full circle to my true calling of combining the art and science of curating elegant experiences for others.
-            <br/><br/>
-            Now, innovating and disrupting legacy industries allows me to build my craft around what I love most: 
-            transforming ambitious ideas into seamlessly ever-evolving products. 
-            <br/><br/>
-            When I’m not coding, you’ll find me taking advantage of all the majestic activities Colorado has to offer, 
-            from biking and hiking in the summer to skiing and hibernating in the winter.  
+            <br />
+            <br />
+            I’m a full-stack developer with an emphasis on usability and
+            standards to solve complex problems through cutting-edge
+            technologies and proven conventions. My track record for delivering
+            well-crafted and -designed software showcases my dedication to
+            producing user-focused and performant solutions.
+            <br />
+            <br />
+            My collegiate career began at CU studying architecture and quickly
+            pivoted after discovering my passion for healthcare and
+            entrepreneurial pursuits. Designing and managing the creation of
+            software as well as exposure to the growing startup ecosystem in
+            Colorado brought me full circle to my true calling of combining the
+            art and science of curating elegant experiences for others.
+            <br />
+            <br />
+            Now, innovating and disrupting legacy industries allows me to build
+            my craft around what I love most: transforming ambitious ideas into
+            seamlessly ever-evolving products.
+            <br />
+            <br />
+            When I’m not coding, you’ll find me taking advantage of all the
+            majestic activities Colorado has to offer, from biking and hiking in
+            the summer to skiing and hibernating in the winter.
           </p>
           {close}
         </article>
 
-        <article id="work" 
-          className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
-          style={{display:'none'}}>
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>
-
-          </p>
+          <span className="image main">
+            <img src={pic02} alt="" />
+          </span>
+          <p></p>
           {close}
         </article>
 
-        <article id="social" 
-          className={`${this.props.article === 'social' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
-          style={{display:'none'}}>
+        <article
+          id="social"
+          className={`${this.props.article === 'social' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Social</h2>
           <ul className="icons">
-            <li><a href="https://twitter.com/eric_fransen" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="https://github.com/ericfransen" className="icon fa-github"><span className="label">GitHub</span></a></li>
-            <li><a href="https://www.linkedin.com/in/franseneric/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
-            <li><a href="https://www.pinterest.com/ericfransen/" className="icon fa-pinterest"><span className="label">Pinterest</span></a></li>
-            <li><a href="https://plus.google.com/101055949734132496398/" className="icon fa-google"><span className="label">Google+</span></a></li>
+            <li>
+              <a
+                href="https://twitter.com/eric_fransen"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/ericfransen"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/franseneric/"
+                className="icon fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.pinterest.com/ericfransen/"
+                className="icon fa-pinterest"
+              >
+                <span className="label">Pinterest</span>
+              </a>
+            </li>
           </ul>
-            {close}
+          {close}
         </article>
 
-        <article id="contact" 
-          className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} 
-            style={{display:'none'}}>
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Contact</h2>
           <form method="post" data-netlify="true" method="POST">
             <div className="field half first">
@@ -76,16 +139,24 @@ class Main extends React.Component {
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4" required ></textarea>
+              <textarea
+                name="message"
+                id="message"
+                rows="4"
+                required
+              ></textarea>
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
             </ul>
           </form>
           {close}
         </article>
-
       </div>
     )
   }
@@ -96,7 +167,7 @@ Main.propTypes = {
   article: React.PropTypes.string,
   articleTimeout: React.PropTypes.bool,
   onCloseArticle: React.PropTypes.func,
-  timeout: React.PropTypes.bool
+  timeout: React.PropTypes.bool,
 }
 
 export default Main
